59e79a08dfededc2d539b6eef92621b6bed3ecd1
KirillPikov <pikov_kirya@bk.ru>
2019-11-11 17:28:17 +0400

    Create ConverterTest.java

---
+++ src/test/java/ConverterTest.java
@@ -0,0 +1,38 @@
+import org.junit.Assert;
+import org.junit.Test;
+
+import static org.junit.Assert.*;
+
+public class ConverterTest {
+
+    @Test
+    public void toRubles() throws WrongValueException {
+        Assert.assertEquals(String.valueOf(100.0 * Settings.cours)
+                        .replace('.', Settings.split.charAt(0)) + "p",
+                Converter.toRubles("$100")
+        );
+        Assert.assertEquals(String.valueOf(129.562 * Settings.cours)
+                        .replace('.', Settings.split.charAt(0)) + "p",
+                Converter.toRubles("$129" + Settings.split + "562")
+        );
+    }
+
+    @Test
+    public void toDollars() throws WrongValueException {
+        Assert.assertEquals("$" + String.valueOf(100.0 / Settings.cours)
+                        .replace('.', Settings.split.charAt(0)),
+                Converter.toDollars("100p")
+        );
+        Assert.assertEquals("$" + String.valueOf(6848.5662 / Settings.cours)
+                        .replace('.', Settings.split.charAt(0)),
+                Converter.toDollars("6848" + Settings.split + "5662p")
+        );
+    }
+
+    @Test
+    public void getDoubleValue() throws WrongValueException {
+        Assert.assertEquals(158.314, Converter.getDoubleValue("$158" + Settings.split + "314"), 0);
+        Assert.assertEquals(1598.31654, Converter.getDoubleValue("1598" + Settings.split + "31654p"), 0);
+        Assert.assertEquals(1598.0, Converter.getDoubleValue("1598" + Settings.split + "00p"), 0);
+    }
+}
