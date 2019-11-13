b56dd55ea09d6f601e331196f3042c0a302ef1ec
KirillPikov <pikov_kirya@bk.ru>
2019-11-13 18:20:41 +0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,61 +1,7 @@
 public class Main {
 
-    public static void main(String[] args) throws WrongValueException {
-        String test = "toDollars(toDollars(59p + toDollars(120p - $2) + 61p - toRubles($1)) + toRubles($3))";
-        System.out.println(calculate(test));
-    }
-
-    public static String calculate(String expression) throws WrongValueException {
-        String result = expression;
-        if(expression.startsWith("toDollars(")) {
-            int indexEndInnerExp = getIndexEndInnerExp(expression);
-            result = Converter.toDollars(
-                    calculate(expression.substring(10, indexEndInnerExp))
-            );
-            if(indexEndInnerExp < expression.length() - 1) {
-                if (expression.charAt(indexEndInnerExp + 2) == '+') {
-                    result = Calculator.sum(result, calculate(expression.substring(indexEndInnerExp + 4)));
-                }
-                if (expression.charAt(indexEndInnerExp + 2) == '-') {
-                    result = Calculator.sub(result, calculate(expression.substring(indexEndInnerExp + 4)));
-                }
-            }
-        } else
-        if(expression.startsWith("toRubles(")) {
-            int indexEndInnerExp = getIndexEndInnerExp(expression);
-            result = Converter.toRubles(
-                    calculate(expression.substring(9, indexEndInnerExp))
-            );
-            if(indexEndInnerExp < expression.length() - 1) {
-                if (expression.charAt(indexEndInnerExp + 2) == '+') {
-                    result = Calculator.sum(result, calculate(expression.substring(indexEndInnerExp + 4)));
-                }
-                if (expression.charAt(indexEndInnerExp + 2) == '-') {
-                    result = Calculator.sub(result, calculate(expression.substring(indexEndInnerExp + 4)));
-                }
-            }
-        } else {
-            int indexFirstSpace = expression.indexOf(' ');
-            if(expression.charAt(indexFirstSpace + 1) == '+') {
-                result = Calculator.sum(expression.substring(0, indexFirstSpace), calculate(expression.substring(indexFirstSpace + 3)));
-            }
-            if(expression.charAt(indexFirstSpace + 1) == '-') {
-                result = Calculator.sub(expression.substring(0, indexFirstSpace), calculate(expression.substring(indexFirstSpace + 3)));
-            }
-        }
-        return result;
-    }
-
-    public static int getIndexEndInnerExp(String expression) {
-        char[] charArray = expression.substring(10).toCharArray();
-        int brakesCount = 1;
-        int indexEndInnerExp = 10;
-        for (char ch: charArray) {
-            if(ch == '(') brakesCount++;
-            if(ch == ')') brakesCount--;
-            if(brakesCount == 0) break;
-            indexEndInnerExp++;
-        }
-        return indexEndInnerExp;
+    public static void main(String[] args) throws WrongValueException, WrongExpressionFormatException {
+        String testExpression = "350,366p - toRubles(toDollars(120,66p - $1,63)) + 120p - 5p - 1p - toRubles(15p - toRubles(3p + toRubles(2p))) - 1p";
+        System.out.println(Calculator.calculateExpression(testExpression));
     }
 }
