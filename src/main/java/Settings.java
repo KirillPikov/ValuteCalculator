5cef6c69a306ce8c4ef6e4711242414f0ca9a13a
KirillPikov <pikov_kirya@bk.ru>
2019-11-13 18:35:32 +0400

    Update Settings.java

--- src/main/java/Settings.java
+++ src/main/java/Settings.java
@@ -1,4 +1,13 @@
+/**
+ * Класс, содержащий настройки приложения.
+ */
 public class Settings {
+    /**
+     * Курс доллара в рублях.
+     */
     public static final int cours = 60;
+    /**
+     * Разделитель в строковом представлении значений.
+     */
     public static final String split = ",";
 }
