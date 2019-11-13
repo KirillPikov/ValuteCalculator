484a485adcc25d405566a3e60effbed8f345bb01
KirillPikov <pikov_kirya@bk.ru>
2019-11-13 18:21:00 +0400

    Update ConverterTest.java

--- src/test/java/ConverterTest.java
+++ src/test/java/ConverterTest.java
@@ -6,7 +6,7 @@ import static org.junit.Assert.*;
 public class ConverterTest {
 
     @Test
-    public void toRubles() throws WrongValueException {
+    public void toRubles() throws WrongValueException, WrongExpressionFormatException {
         Assert.assertEquals(String.valueOf(100.0 * Settings.cours)
                         .replace('.', Settings.split.charAt(0)) + "p",
                 Converter.toRubles("$100")
@@ -18,7 +18,7 @@ public class ConverterTest {
     }
 
     @Test
-    public void toDollars() throws WrongValueException {
+    public void toDollars() throws WrongValueException, WrongExpressionFormatException {
         Assert.assertEquals("$" + String.valueOf(100.0 / Settings.cours)
                         .replace('.', Settings.split.charAt(0)),
                 Converter.toDollars("100p")
@@ -30,7 +30,7 @@ public class ConverterTest {
     }
 
     @Test
-    public void getDoubleValue() throws WrongValueException {
+    public void getDoubleValue() throws WrongValueException, WrongExpressionFormatException {
         Assert.assertEquals(158.314, Converter.getDoubleValue("$158" + Settings.split + "314"), 0);
         Assert.assertEquals(1598.31654, Converter.getDoubleValue("1598" + Settings.split + "31654p"), 0);
         Assert.assertEquals(1598.0, Converter.getDoubleValue("1598" + Settings.split + "00p"), 0);
