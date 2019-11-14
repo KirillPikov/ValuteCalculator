cf4b89649b25b437f9584ef8a8037a6aa6dcf87b
KirillPikov <pikov_kirya@bk.ru>
2019-11-14 19:43:44 +0400

    Update Main.java

--- src/main/java/Main.java
+++ src/main/java/Main.java
@@ -1,7 +1,7 @@
 public class Main {
 
     public static void main(String[] args) throws WrongValueException, WrongExpressionFormatException {
-        String testExpression = "350,366p - toRubles(toDollars(120,66p - $1,63)) + 120p - 5p - 1p - toRubles(15p - toRubles(3p + toRubles(2p))) - 1p";
+        String testExpression = "$8 - toDollars(350,366p - toRubles(toDollars(120,66p - $1,63)) + 120p - 5p - 1p - toRubles(15p - toRubles(3p + toRubles(2p))) - 1p)";
         System.out.println(Calculator.calculateExpression(testExpression));
     }
 }
