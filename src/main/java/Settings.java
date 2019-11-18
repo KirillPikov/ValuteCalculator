6a2f8c1d0dabf620477bca6e6cdb993e9c640821
KirillPikov <pikov_kirya@bk.ru>
2019-11-18 13:14:25 +0400

    Update Settings.java

--- src/main/java/Settings.java
+++ src/main/java/Settings.java
@@ -5,9 +5,13 @@ public class Settings {
     /**
      * Курс доллара в рублях.
      */
-    public static final int cours = 60;
+    public static final int COURS = 60;
     /**
      * Разделитель в строковом представлении значений.
      */
-    public static final String split = ",";
+    public static final String SPLIT = ",";
+    /**
+     * Количество значимых разрядов после запятой.
+     */
+    public static final Integer DIGITS_COUNT_AFTER_COMMA = 4 + 1;
 }
