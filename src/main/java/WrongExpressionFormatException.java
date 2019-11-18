91237faa5fb91578a5aea59cdbe3ddf7374fb39b
KirillPikov <pikov_kirya@bk.ru>
2019-11-18 13:14:19 +0400

    Update WrongExpressionFormatException.java

--- src/main/java/WrongExpressionFormatException.java
+++ src/main/java/WrongExpressionFormatException.java
@@ -1,3 +1,6 @@
+/**
+ * Исключение означает, что выражение является синтаксически некорректным с точки зрения задачи.
+ */
 public class WrongExpressionFormatException extends Exception {
     public WrongExpressionFormatException(String message) {
         super(message);
