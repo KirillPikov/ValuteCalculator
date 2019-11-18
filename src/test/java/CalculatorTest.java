8f7c478605d9171efcc5d499f26473490612d84e
KiryaPikoff <har4ok@bk.ru>
2019-11-18 07:04:36 +0300

    Update CalculatorTest.java

--- src/test/java/CalculatorTest.java
+++ src/test/java/CalculatorTest.java
@@ -23,34 +23,34 @@ public class CalculatorTest {
     @Test
     public void calculateExpression() throws WrongValueException, WrongExpressionFormatException {
         Assert.assertEquals(
-                "$97",
+                "$97,6833",
                 Calculator.calculateExpression(
                         "toDollars(737p + toRubles($85,4))")
-                        .split(Settings.split)[0]
+
         );
         Assert.assertEquals(
-                "1934",
+                "1934,4499p",
                 Calculator.calculateExpression(
                 "35,87p + toDollars(350p + $25,81)")
-                .split(Settings.split)[0]
+
         );
         Assert.assertEquals(
-                "764",
+                "764,4490p",
                 Calculator.calculateExpression(
                         "35,87p + toDollars(350p + $25,81 - 15p) - toRubles($19,25)")
-                        .split(Settings.split)[0]
+
         );
         Assert.assertEquals(
-                "7964",
+                "7964,329p",
                 Calculator.calculateExpression(
                         "35,87p + toDollars(350p + $25,81 - 15p) - toRubles($19,25) + toRubles(toDollars($150 - toDollars(1800,15234p)))")
-                        .split(Settings.split)[0]
+
         );
         Assert.assertEquals(
-                "7683",
+                "7683,2840p",
                 Calculator.calculateExpression(
                         "35,87p + toDollars(350p + $25,81 - 15p) - toRubles($19,25) + toDollars($6,12 + 569,58p + toRubles(toDollars($150 - toDollars(1800,15234p) - 895p - $32))) + 1597,256p")
-                        .split(Settings.split)[0]
+
         );
     }
 }
