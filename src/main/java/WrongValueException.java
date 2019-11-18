d27c14c01942edc8712a6a20d6ff118329fe263b
KirillPikov <pikov_kirya@bk.ru>
2019-11-18 13:14:21 +0400

    Update WrongValueException.java

--- src/main/java/WrongValueException.java
+++ src/main/java/WrongValueException.java
@@ -1,3 +1,6 @@
+/**
+ * Исключение означает, что в ходе вычислений было получено отрицательное число.
+ */
 public class WrongValueException extends Exception {
     public WrongValueException(String message) {
         super(message);
